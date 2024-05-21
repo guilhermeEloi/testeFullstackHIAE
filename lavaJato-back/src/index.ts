b90413c30ad7e3d7f39b9ccd3b1e4681e387a8ee
guilhermeEloi <gui.eloi.silva@gmail.com>
2024-05-21 11:08:55 -0300

    Adjust CORS

--- lavaJato-back/src/index.ts
+++ lavaJato-back/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
